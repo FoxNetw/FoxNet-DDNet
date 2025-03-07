c3e29bcfb9f586d0269f86efa8605e2da2aa2c4e
qxdFox <jusi.p@icloud.com>
2025-03-07 18:59:40 +0100

    idk where this came from

--- src/game/server/gamecontroller.cpp
+++ src/game/server/gamecontroller.cpp
@@ -526,11 +526,6 @@ void IGameController::HandleCharacterTiles(CCharacter *pChr, int MapIndex)
 	// Do nothing by default
 }
 
-void IGameController::HandleCharacterQuad(CCharacter *pChr, CQuad *pQuad)
-{
-	// Do nothing by default
-}
-
 void IGameController::DoWarmup(int Seconds)
 {
 	if(Seconds < 0)
