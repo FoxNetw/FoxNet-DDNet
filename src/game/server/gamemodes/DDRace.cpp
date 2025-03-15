01dd78b282a6acbc052f2d72b6dccf97afb823cc
qxdFox <jusi.p@icloud.com>
2025-03-15 22:55:17 +0100

    show what client a person is using on join

--- src/game/server/gamemodes/DDRace.cpp
+++ src/game/server/gamemodes/DDRace.cpp
@@ -9,6 +9,7 @@
 #include <game/server/gamecontext.h>
 #include <game/server/player.h>
 #include <game/server/score.h>
+#include <game/version.h>
 
 CGameControllerDDRace::CGameControllerDDRace(class CGameContext *pGameServer) :
 	IGameController(pGameServer)
@@ -127,11 +128,18 @@ void CGameControllerDDRace::OnPlayerConnect(CPlayer *pPlayer)
 
 	if(!Server()->ClientPrevIngame(ClientId))
 	{
+		IServer::CClientInfo Info;
+		char PlayerInfo[512] = "";
+		if(Server()->GetClientInfo(ClientId, &Info) && Info.m_GotDDNetVersion)
+		{
+			str_format(PlayerInfo, sizeof(PlayerInfo), " (%s)", Info.m_pDDNetVersionStr);
+		}
+
 		char aBuf[512];
-		str_format(aBuf, sizeof(aBuf), "'%s' entered and joined the %s", Server()->ClientName(ClientId), GetTeamName(pPlayer->GetTeam()));
+		str_format(aBuf, sizeof(aBuf), "'%s' entered and joined the %s%s", Server()->ClientName(ClientId), GetTeamName(pPlayer->GetTeam()), PlayerInfo);
 		GameServer()->SendChat(-1, TEAM_ALL, aBuf, -1, CGameContext::FLAG_SIX);
 
-		GameServer()->SendChatTarget(ClientId, "FoxNetwork");
+		GameServer()->SendChatTarget(ClientId, "FoxNetwork mod " GAME_VERSION);
 		GameServer()->SendChatTarget(ClientId, "This Server serves as a Testing Server");
 		GameServer()->SendChatTarget(ClientId, "Expect Random Restarts or bans");
 		GameServer()->SendChatTarget(ClientId, "Cheaters will still be banned.");
